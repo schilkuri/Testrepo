20e60b0213eced3ac8fa8f111a274895d8daf61b
schilkuri <schilkuri@gmail.com>
2017-10-24 07:27:54 +0000

    java modified number1

--- demo.java
+++ demo.java
@@ -7,3 +7,11 @@
 <td>User Name</td><td><input type="text" name="t1" ></td>
 </tr>
 <td>Password</td>
+<td><input type=""password"" name="t2"></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" name="b1" value="LogIn"></td>
+</tr>
+</table>
+<%
