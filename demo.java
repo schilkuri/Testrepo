543effbaa5a5fd6dc02a5ee6b00066d1629b5fc3
schilkuri <schilkuri@gmail.com>
2017-10-24 07:18:30 +0000

    This is a java code 1

---
+++ demo.java
@@ -0,0 +1,9 @@
+<%@ page language ="java" import="java.sql.*" %>
+<html>
+<body bgcolor="pink">
+<form name="f1" method="post">
+<table>
+<tr>
+<td>User Name</td><td><input type="text" name="t1" ></td>
+</tr>
+<td>Password</td>
